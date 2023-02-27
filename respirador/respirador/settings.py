fa38a696c5e368b5fd673c24f2cd016929892456
Matias-sh <devmatiasbritez@gmail.com>
2023-02-27 10:54:19 -0300

    agregado el dns de la instancia de aws a allowed_hosts

--- respirador/respirador/settings.py
+++ respirador/respirador/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tmwoy8ltc04fg)!!(fzwb-z78#+y5&$pn9qfs+v&w@+dik@3mb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ec2-54-172-121-27.compute-1.amazonaws.com]
 
 
 # Application definition
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
